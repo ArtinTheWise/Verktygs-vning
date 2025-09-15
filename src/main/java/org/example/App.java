539779074a538f01e711157ca9474aa4ef186c2a
axba3072 <axba3072@student.su.se>
2025-09-15 11:05:59 +0200

    Konflikt test

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,6 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "HejHejHej!" );
+        System.out.println( "Halloj halloj" );
     }
+
+    
 }
